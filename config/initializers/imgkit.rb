8e75aaf593ea00d1c6e81f0a339e46a714135d92
Kiwiupover <kiwiupover@gmail.com>
2011-12-13 15:31:38 -0800

    moved the wk file

--- config/initializers/imgkit.rb
+++ config/initializers/imgkit.rb
@@ -1,6 +1,6 @@
 # config/initializers/imgkit.rb
 IMGKit.configure do |config|
-  config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage-amd64').to_s if ENV['RACK_ENV'] == 'production'
+  config.wkhtmltoimage = Rails.root.join('lib', 'wkhtmltoimage-amd64').to_s if ENV['RACK_ENV'] == 'production'
   config.wkhtmltoimage = '/usr/local/bin/wkhtmltoimage' if ENV['RACK_ENV'] == 'development' 
   config.default_options = {
     :quality => 100,
