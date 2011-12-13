41525eac1224a2e2a0d3b166d1847a8fadc662ab
Kiwiupover <kiwiupover@gmail.com>
2011-12-13 15:15:23 -0800

    Fixed Migrations

--- db/migrate/20111118202916_add_landing_page_type_id_to_landing_pages.rb
+++ db/migrate/20111118202916_add_landing_page_type_id_to_landing_pages.rb
@@ -1,6 +1,6 @@
 class AddLandingPageTypeIdToLandingPages < ActiveRecord::Migration
   def change
-    add_column :landing_pages, :landing_page_type_id, :interger
-    add_column :landing_pages, :industry_id, :interger
+    add_column :landing_pages, :landing_page_type_id, :integer
+    add_column :landing_pages, :industry_id, :integer
   end
 end
